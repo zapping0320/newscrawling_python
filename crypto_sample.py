ac4a1191188f1999b7b0897ae16680234adc2553
John DongHyun Kim <zapping0320@gmail.com>
2022-01-25 13:22:03 +0900

    검색 결과에서 시간 스트링 처리 추가함

--- crypto_sample.py
+++ crypto_sample.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-token = "{proper workspace totken value}"
+token = "{token}"
 channel = "#news_crawler01"
 
 raw = requests.get("https://search.naver.com/search.naver?where=news&query=암호화폐 세금",
@@ -10,19 +10,21 @@ html = BeautifulSoup(raw.text, "html.parser")
 
 articles = html.select("ul.list_news > li")
 
-#print(articles[0])
 for ar in articles:
     times  = ar.select('li > div > div > div > div > span')
     print(times[0].get_text())
+    timestring = times[0].get_text()
+    
+    if timestring.find("시간 전") != -1 or timestring.find("분 전") != -1:
+        titles = ar.select('li > div > div > a')
+        print(titles[0].get_text())
+        print(titles[0]["href"])
+   
+        requests.post("https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer "+token},
+        data={"channel": channel,"text": titles[0].get_text()})
 
-    titles = ar.select('li > div > div > a')
-    print(titles[0].get_text())
-    print(titles[0]["href"])
-    requests.post("https://slack.com/api/chat.postMessage",
-    headers={"Authorization": "Bearer "+token},
-    data={"channel": channel,"text": titles[0].get_text()})
-
-    requests.post("https://slack.com/api/chat.postMessage",
-    headers={"Authorization": "Bearer "+token},
-    data={"channel": channel,"text": titles[0]["href"]})
+        requests.post("https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer "+token},
+        data={"channel": channel,"text": titles[0]["href"]})
 
