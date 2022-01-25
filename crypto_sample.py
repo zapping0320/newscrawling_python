fd78b5593914ea1bec11abfb1342b8be9abc01f5
John DongHyun Kim <zapping0320@gmail.com>
2022-01-25 12:33:13 +0900

    slack 특정 채널에 봇을 설치하고 그 봇으로 메시지 보내는 기능 추가함

--- crypto_sample.py
+++ crypto_sample.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
+token = "{proper workspace totken value}"
+channel = "#news_crawler01"
+
 raw = requests.get("https://search.naver.com/search.naver?where=news&query=암호화폐 세금",
                    headers={'User-Agent':'Mozilla/5.0'})
 html = BeautifulSoup(raw.text, "html.parser")
@@ -15,5 +18,11 @@ for ar in articles:
     titles = ar.select('li > div > div > a')
     print(titles[0].get_text())
     print(titles[0]["href"])
-    
+    requests.post("https://slack.com/api/chat.postMessage",
+    headers={"Authorization": "Bearer "+token},
+    data={"channel": channel,"text": titles[0].get_text()})
+
+    requests.post("https://slack.com/api/chat.postMessage",
+    headers={"Authorization": "Bearer "+token},
+    data={"channel": channel,"text": titles[0]["href"]})
 
